3dff0a26cf2e5d02a4ad31392e59908971d0b8ff
Eric Roberts <baldeagle072@gmail.com>
2015-01-29 14:32:27 -0800

    dev

--- web_thermostat/script.js
+++ web_thermostat/script.js
@@ -16,8 +16,12 @@ $('.tstat-cooling .up-btn').click(function() {
 	
 });
 
+function onClick() {
+	alert("test2");
+};
+
 function getCommandUrl(){
 	var url = "command/?" + window.location.href.slice(window.location.href.indexOf('?') + 1)
 	alert(url);
 	return url;
-}
+};
